58f9a727f0af2d04e8a864b121c6101156d2f638
Muyinda Rogers <moverr@gmail.com>
2019-10-29 19:24:53 +0300

    :Debuging :

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 class Application:
-    def main():
-        echo "Let me know oaea "
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def main(self):
+        print("Let me know oaea ")
 
 
 
