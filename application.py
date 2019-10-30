090137592704256282764a1ee3766d08de73ed30
Muyinda Rogers <moverr@gmail.com>
2019-10-30 16:42:18 +0300

    :Debuging

--- application.py
+++ application.py
@@ -4,33 +4,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
-def importDataset(self, url, names):
-    self.dataset = pd.read_csv(url, usecols=[0, 1, 2], names=names)
+dataset = ""
+resultdataset = ""
+x_train, x_test, y_train, y_test = ""
 
-def importResultsDataset(self, url, names):
-    self.resultdataset = pd.read_csv(url, usecols=[3], names=names)
+def importDataset( url, names):
+    dataset = pd.read_csv(url, usecols=[0, 1, 2], names=names)
+
+def importResultsDataset( url, names):
+    resultdataset = pd.read_csv(url, usecols=[3], names=names)
 
 def splitTrainingData(self):
-    self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.dataset,self.resultdataset,random_state = 0)
+    x_train, x_test, y_train, y_test = train_test_split(dataset,resultdataset,random_state = 0)
 
 def knnClassifier(self):
     knn = KNeighborsClassifier(n_neighbors=1)
-    knn.fit(self.x_train, self.y_train)
+    knn.fit(x_train, y_train)
     x_new = np.array([[161.29, 48.987936, 1]])
     return knn.predict(x_new)
 
 def main(self):
-    self.names = ['Height', 'Weight', 'Gender']
-    self.resultnames = ['AgeGroup']
+    names = ['Height', 'Weight', 'Gender']
+    resultnames = ['AgeGroup']
 
-    self.dataseturl = "./dataset.csv"
-    self.importDataset(dataseturl, self.names)
-#         self.importDataset(dataseturl,self.resultnames)
+    dataseturl = "./dataset.csv"
+    importDataset(dataseturl, names)
+#         importDataset(dataseturl,resultnames)
 
 
-#         print("Data set \n {} ".format(self.dataset[:10]))
+#         print("Data set \n {} ".format(dataset[:10]))
 
-#         print("Result data set \n {} ".format(self.resultdataset[:20]))
+#         print("Result data set \n {} ".format(resultdataset[:20]))
 
 
 #         print("Panda Version  :  {}".format(pd.__version__))
@@ -38,7 +42,7 @@ def main(self):
 
 
 # # split data using sklean
-#         prediction = self.splitTrainingData()
+#         prediction = splitTrainingData()
 
 #         # K Nearest Neighborhood
 
