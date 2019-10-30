bf49fe31cd87a66c76fc4262a4784e8b338bc7bc
Muyinda Rogers <moverr@gmail.com>
2019-10-30 15:17:02 +0300

    Get the Daase and Result dataset from the excel sheet

--- application.py
+++ application.py
@@ -9,10 +9,17 @@ class Application:
     def main(self):
         dataseturl = "./dataset.csv"
         names = ['Height', 'Weight', 'Gender']
+        resultnames = ['AgeGroup']
+        
         dataset = pd.read_csv(dataseturl,usecols=[0,1,2], names=names)
 
+        # record set 
+        resultdataset = pd.read_csv(dataseturl,usecols=[3], names=resultnames)
+        
         print("Data set \n {} ".format(dataset[:10]))
 
+        print("Result data set \n {} ".format(resultdataset[:20]))
+        
 
         print("Panda Version  :  {}".format(pd.__version__))
         print("Let me know oaea ")
