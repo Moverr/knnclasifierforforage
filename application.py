f35395043c3c1cf2062ed3b1bd70ff136ddc642d
Muyinda Rogers <moverr@gmail.com>
2019-10-30 16:20:50 +0300

    :Debuging

--- application.py
+++ application.py
@@ -9,27 +9,28 @@ import numpy as np
 class Application:
     def __init__(self):
         self.names = ['Height', 'Weight', 'Gender']
+        self.resultnames = ['AgeGroup']
          
     def importDataset(self,url,names):
         self.dataset = pd.read_csv(url,usecols=[0,1,2], names=names)
 
-         
+    def importResultsDataset(self,url,names):
+        self.resultdataset = pd.read_csv(url,usecols=[3], names=names)
+    
+    def splitTrainingData(self):
+        self.x_train,self.x_test,self.y_train,self.y_test = train_test_split(self.dataset,self.resultdataset,random_state = 0)
+
+
 
     def main(self):
         dataseturl = "./dataset.csv"
         self.importDataset(dataseturl,self.names)
-
+        self.importDataset(dataseturl,self.resultnames)
         
-        resultnames = ['AgeGroup']
-        
-      
+ 
+        print("Data set \n {} ".format(self.dataset[:10]))
 
-        # record set 
-        resultdataset = pd.read_csv(dataseturl,usecols=[3], names=resultnames)
-        
-        print("Data set \n {} ".format(dataset[:10]))
-
-        print("Result data set \n {} ".format(resultdataset[:20]))
+        print("Result data set \n {} ".format(self.resultdataset[:20]))
         
 
         print("Panda Version  :  {}".format(pd.__version__))
