9909f2488158566eb4d2df7e2a19d6a4fe94aeaa
Muyinda Rogers <moverr@gmail.com>
2019-10-30 16:38:42 +0300

    :Debugigng

--- application.py
+++ application.py
@@ -32,30 +32,30 @@ class Application:
         self.names = ['Height', 'Weight', 'Gender']
         self.resultnames = ['AgeGroup']
       
-        dataseturl = "./dataset.csv"
-        self.importDataset(dataseturl,self.names)
-        self.importDataset(dataseturl,self.resultnames)
+        self.dataseturl = "./dataset.csv"
+#         self.importDataset(dataseturl,self.names)
+#         self.importDataset(dataseturl,self.resultnames)
         
  
-        print("Data set \n {} ".format(self.dataset[:10]))
+#         print("Data set \n {} ".format(self.dataset[:10]))
 
-        print("Result data set \n {} ".format(self.resultdataset[:20]))
+#         print("Result data set \n {} ".format(self.resultdataset[:20]))
         
 
-        print("Panda Version  :  {}".format(pd.__version__))
-        print("Let me know oaea ")
+#         print("Panda Version  :  {}".format(pd.__version__))
+#         print("Let me know oaea ")
 
 
-# split data using sklean
-        prediction = self.splitTrainingData()
+# # split data using sklean
+#         prediction = self.splitTrainingData()
 
-        # K Nearest Neighborhood 
+#         # K Nearest Neighborhood 
        
 
-        ageGroup = np.array(['Child','Teen','Adult'])
-        print(" Prediction {}".format(ageGroup[prediction]))
+        # ageGroup = np.array(['Child','Teen','Adult'])
+        # print(" Prediction {}".format(ageGroup[prediction]))
 
-
+    
 
 
   
