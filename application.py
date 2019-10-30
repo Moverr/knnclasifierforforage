f974a8a453379179d0b03c45990327de99d6ed5b
Muyinda Rogers <moverr@gmail.com>
2019-10-30 16:55:51 +0300

    :Debugging

--- application.py
+++ application.py
@@ -3,9 +3,7 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
-
-x_train, x_test, y_train, y_test = ""
-
+ 
 def importDataset( url, names):
     dataset = pd.read_csv(url, usecols=[0, 1, 2], names=names)
     return dataset
@@ -18,21 +16,21 @@ def splitTrainingData(dataset,resultdataset):
     x_train, x_test, y_train, y_test = train_test_split(dataset,resultdataset,random_state = 0)
     return  x_train, x_test, y_train, y_test
 
-def knnClassifier():
+def knnClassifier(x_train,y_train):
     knn = KNeighborsClassifier(n_neighbors=1)
     knn.fit(x_train, y_train)
     x_new = np.array([[161.29, 48.987936, 1]])
     return knn.predict(x_new)
 
-def main(self):
+def main():
     names = ['Height', 'Weight', 'Gender']
     resultnames = ['AgeGroup']
 
     dataseturl = "./dataset.csv"
     dataset = importDataset(dataseturl, names)
-    resultdataset =  importDataset(dataseturl,resultnames)
+    resultdataset =  importResultsDataset(dataseturl,resultnames)
 
-    print("Data set \n {} ".format(dataset[:10]))
+    # print("Data set \n {} ".format(dataset[:10]))
 
 #         print("Result data set \n {} ".format(resultdataset[:20]))
 
