0ff098baa0f502da02563a5a4858c89060b2b1f9
Muyinda Rogers <moverr@gmail.com>
2019-11-25 16:27:09 +0300

    :Debugging :

--- decissiontree.py
+++ decissiontree.py
@@ -2,4 +2,8 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_breast_cancer
 
+from sklearn.model_selection import train_test_split
 cancer = load_breast_cancer()
+
+x_train,x_test,y_train, y_test = train_test_split(cancer.data,cancer.target,stratify=cancer.target,random_state= 42)\
+
